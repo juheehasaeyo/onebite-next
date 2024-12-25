e67ae7237d143082fe46049d3b157cabfc6bf8f7
조주희 <carla_cho@naver.com>
2024-12-25 18:49:27 +0900

    fix: 에러 처리

--- section09/src/app/(with-searchbar)/layout.tsx
+++ section09/src/app/(with-searchbar)/layout.tsx
@@ -1,15 +1,14 @@
 import { ReactNode } from "react";
 import Searchbar from "../../components/searchbar";
+import { Suspense } from "react";
 
-export default function Layout({
-  children,
-}: {
-  children: ReactNode;
-}) {
-  return (
-    <div>
-      <Searchbar />
-      {children}
-    </div>
-  );
+export default function Layout({ children }: { children: ReactNode }) {
+    return (
+        <div>
+            <Suspense>
+                <Searchbar />
+            </Suspense>
+            {children}
+        </div>
+    );
 }
